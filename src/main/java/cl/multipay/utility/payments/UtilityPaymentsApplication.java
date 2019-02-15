9807bc09a9c1a5a6c61300697103da5dcd508872
Carlos Izquierdo <cizhe@me.com>
2019-02-15 15:43:14 -0300

    test variables

--- src/main/java/cl/multipay/utility/payments/UtilityPaymentsApplication.java
+++ src/main/java/cl/multipay/utility/payments/UtilityPaymentsApplication.java
@@ -1,13 +1,34 @@
 package cl.multipay.utility.payments;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 public class UtilityPaymentsApplication
 {
+
+	@Value("${spring.datasource.url}")
+	private String dsUrl;
+
+	@Value("${spring.datasource.username}")
+	private String dsUser;
+
+	@Value("${spring.datasource.password}")
+	private String dsPass;
+
+	@Bean
+	public String test()
+	{
+		System.out.println("--> dsUrl: "+ dsUrl);
+		System.out.println("--> dsUser: "+ dsUser);
+		System.out.println("--> dsPass: "+ dsPass);
+		return "asd";
+	}
+
 	public static void main(final String[] args)
 	{
 		SpringApplication.run(UtilityPaymentsApplication.class, args);
