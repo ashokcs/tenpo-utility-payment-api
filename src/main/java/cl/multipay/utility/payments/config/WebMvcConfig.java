95b4511a8fd2e12a8c74f3492870b6e48bec8192
Carlos Izquierdo <cizhe@me.com>
2019-02-20 10:57:22 -0300

    disable cors staging

---
+++ src/main/java/cl/multipay/utility/payments/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package cl.multipay.utility.payments.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("staging")
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer
+{
+	@Override
+	public void addCorsMappings(final CorsRegistry registry)
+	{
+		registry.addMapping("/**");
+	}
+}
