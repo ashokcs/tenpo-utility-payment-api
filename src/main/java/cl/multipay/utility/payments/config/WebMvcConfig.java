8c41c097b4f5a3f89f799d9ec51434bf49544216
Carlos Izquierdo <cizhe@me.com>
2019-02-21 10:32:47 -0300

    remove enablewebmvc annotatio

--- src/main/java/cl/multipay/utility/payments/config/WebMvcConfig.java
+++ src/main/java/cl/multipay/utility/payments/config/WebMvcConfig.java
@@ -3,12 +3,10 @@ package cl.multipay.utility.payments.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Profile("staging")
-@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer
 {
 	@Override
