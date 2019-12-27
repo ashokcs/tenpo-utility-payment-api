912031c457752e543d8d692e6654c2173af04db7
Carlos Izquierdo <cizhe@me.com>
2019-12-27 11:54:17 -0300

    options endpoints

--- src/main/java/cl/tenpo/utility/payments/controller/OptionController.java
+++ src/main/java/cl/tenpo/utility/payments/controller/OptionController.java
@@ -1,16 +1,22 @@
 package cl.tenpo.utility.payments.controller;
 
+import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
+
+import javax.validation.Valid;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.tenpo.utility.payments.entity.Option;
+import cl.tenpo.utility.payments.object.OptionsRequest;
 import cl.tenpo.utility.payments.repository.OptionRepository;
 
 @RestController
@@ -35,30 +41,28 @@ public class OptionController
 		}
 	}
 
-	/*
 	@PutMapping("/v1/utility-payments/options")
 	public ResponseEntity<?> optionsUpdate(
-		@RequestHeader(value="x-mine-user-id") final UUID userId
-		@RequestBody)
+		@RequestHeader(value="x-mine-user-id") final UUID userId,
+		@RequestBody @Valid final OptionsRequest request)
 	{
 		final Optional<Option> optional = optionRepository.findByUser(userId);
 		if (optional.isPresent()) {
 			final Option tmp = optional.get();
-			tmp.setRemind(true);
-			tmp.setSuggest(true);
-			tmp.setRemindFrequency(0);
+			tmp.setRemind(request.getRemind());
+			tmp.setSuggest(request.getSuggest());
+			tmp.setRemindFrequency(request.getRemindFrequency());
 			optionRepository.save(tmp);
 			return ResponseEntity.ok(tmp);
 		} else {
 			final Option tmp = new Option();
 			tmp.setUser(userId);
-			tmp.setRemind(true);
-			tmp.setSuggest(true);
-			tmp.setRemindFrequency(0);
+			tmp.setRemind(request.getRemind());
+			tmp.setSuggest(request.getSuggest());
+			tmp.setRemindFrequency(request.getRemindFrequency());
 			tmp.setCreated(OffsetDateTime.now());
 			optionRepository.save(tmp);
 			return ResponseEntity.ok(tmp);
 		}
 	}
-	*/
 }
